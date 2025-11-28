69d7eb648b990581adb2b563d2ce2ff6c9cf7d30
JuanyCastro <juanycastro2002@gmail.com>
2025-11-28 16:01:25 -0300

    Delete src directory

--- src/main/java/com/proyectotalento/tiendaapi/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyectotalento.tiendaapi.repository;
-
-import com.proyectotalento.tiendaapi.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
