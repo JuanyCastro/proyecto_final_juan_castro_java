ea34b126b2044a980e75747540923838a1786faa
JuanyCastro <juanycastro2002@gmail.com>
2025-12-08 21:45:26 -0300

    Implement findByActivoTrue method in PedidoRepository
    Added method to find active pedidos.

--- tiendaapi/src/main/java/com/proyectotalento/tiendaapi/repository/PedidoRepository.java
+++ tiendaapi/src/main/java/com/proyectotalento/tiendaapi/repository/PedidoRepository.java
@@ -4,6 +4,10 @@ import com.proyectotalento.tiendaapi.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByActivoTrue();
 }
+
