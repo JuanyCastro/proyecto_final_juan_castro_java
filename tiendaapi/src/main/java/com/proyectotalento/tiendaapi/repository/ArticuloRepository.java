96f54b7f10750ed9f4f931904c60e498652a2202
JuanyCastro <juanycastro2002@gmail.com>
2025-12-08 21:44:32 -0300

    Update ArticuloRepository.java

--- tiendaapi/src/main/java/com/proyectotalento/tiendaapi/repository/ArticuloRepository.java
+++ tiendaapi/src/main/java/com/proyectotalento/tiendaapi/repository/ArticuloRepository.java
@@ -1,12 +1,13 @@
 package com.proyectotalento.tiendaapi.repository;
 
 import com.proyectotalento.tiendaapi.model.Articulo;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
+    List<Articulo> findByActivoTrue();
 }
 
