04ba0a0de648a54e61b108df331a11fc3b6b32bf
JuanyCastro <juanycastro2002@gmail.com>
2025-12-08 21:38:01 -0300

    Add files via upload

---
+++ src/main/java/com/proyectotalento/tiendaapi/repository/ArticuloRepository.java
@@ -0,0 +1,12 @@
+package com.proyectotalento.tiendaapi.repository;
+
+import com.proyectotalento.tiendaapi.model.Articulo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
+    List<Articulo> findByActivoTrue();
+}
